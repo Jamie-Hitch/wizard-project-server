ba988fd133548d0bb6387107f234dd620a0d66d7
Jamie-Hitch <97847257+Jamie-Hitch@users.noreply.github.com>
2022-10-18 16:40:36 +0100

    Add more routes

--- api.js
+++ api.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
 const db = require("./db");
-
 const api = express();
 
 // Add standard middleware
@@ -10,7 +8,6 @@ api.use(express.json());
 api.use(cors());
 
 // Api routes
-
 // GET route for root of API
 api.get("/", (req, res) => res.send("The Masquerade API: they walk among us."));
 
@@ -23,9 +20,18 @@ api.post("/entities", async (req, res) => {
 
 // DELETE route for entity
 api.delete("/entities/:id", async (req, res) => {
-    const id = req.params.id;
+    const id = parseInt(req.params.id);
     await db.query("DELETE FROM entities WHERE entity_id = $1", [id]);
     res.send(`${id} deleted from the entities database`)
+})
+
+// GET route to return specific entity and related incidents
+api.get("/entities/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const data = await db.query("SELECT * FROM entities as E JOIN incidents as I ON E.entity_id = I.entity_id WHERE entity_name = $1", [id]);
+    console.log(data)
+    res.send(data.rows);
+    res.send(`Showing incidents related to ${id}`)
 })
 
 // POST route to add location
@@ -35,17 +41,25 @@ api.post("/locations", async (req, res) => {
     res.send(`${locationName} added to the locations database`);
 })
 
-
+// DELETE route for location
+api.delete("/locations/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    await db.query("DELETE FROM locations WHERE location_id = $1", [id]);
+    res.send(`${id} deleted from the locations database`)
+})
 
 // POST route to add incident
 api.post("/incidents", async (req, res) => {
-    const { entityId, locationId, description, severity, dateTime} = req.body
+    const { entityId, locationId, description, severity, dateTime } = req.body
     await db.query("INSERT INTO incidents (entity_id, location_id, description, severity, date_time) VALUES ($1, $2, $3, $4, $5)", [entityId, locationId, description, severity, dateTime]);
     res.send(`New incident added to the incidents database`);
 })
 
-
-
-
+// DELETE route for location
+api.delete("/incidents/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    await db.query("DELETE FROM incidents WHERE incident_id = $1", [id]);
+    res.send(`${id} deleted from the incidents database`)
+})
 
 module.exports = api;
